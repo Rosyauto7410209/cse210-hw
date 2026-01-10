a76923acd1bb45454928a6e150705ff2c83f8b00
Rosyauto7410209 <64661777+Rosyauto7410209@users.noreply.github.com>
2026-01-09 17:01:57 -0700

    commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.write("What is your grade percentage? ");
+        string answer = Console.readline();
+        int percent = int.parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter ="B";
+        }
+        else if (percent >= 70)
+        {
+            letter ="C";
+        }
+        else if (percent >= 60)
+        {
+            letter ="D";
+        }
+        else
+        {
+            letter ="F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed.");
+        }
+        else
+        {
+            Console.WriteLine("You failed.");
+        }
+
+
     }
 }
