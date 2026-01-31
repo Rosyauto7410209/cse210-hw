7ed8b266bf4b5c9f44903d543f98510435e3d980
Rosyauto7410209 <64661777+Rosyauto7410209@users.noreply.github.com>
2026-01-31 12:00:31 -0700

    commit all

---
+++ sandbox/Sandbox/Journal.cs
@@ -0,0 +1,100 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+    public Promptgenerator prompts = new Promptgenerator();
+
+
+//----------separation bar-------------------
+    public void AddNewEntry(Entry entry)
+    {
+        _entries.Add(entry);      
+    }
+
+
+
+
+//----------separation bar-------------------
+
+
+    public void DisplayEntries()
+    {
+        //loop through each entry 
+        foreach (Entry entry in _entries)
+        {           
+        entry.display();
+        Console.WriteLine();            
+        }
+        //call its display method    
+
+    }
+
+ public void SaveasFile(string filename)
+{      
+       using (StreamWriter outputfile = new StreamWriter(filename))
+    {
+        foreach (Entry e in _entries)
+        {
+            outputfile.WriteLine($"{e._currentTime}-{e.Prompt}-{e.Response}");
+        }
+     }
+
+}
+//----------separation bar-------------------loading file
+
+   public void LoadaFile(string filename)
+{
+    _entries.Clear();
+    string[] lines = File.ReadAllLines(filename);
+    foreach (string line in lines)
+    {
+        string[] parts = line.Split("-");
+        if (parts.Length == 3)
+        {
+        DateTime date = DateTime.Parse(parts[0]);
+        string prompt = parts[1];
+        string response = parts[2];
+
+        _entries.Add(new Entry (date, prompt, response));
+        }
+
+    }
+}
+
+
+}
+
+
+
+
+
+//----------separation bar-------------------Done
+
+public class Entry
+{
+    public DateTime _currentTime;
+    public string Response;
+    public string Prompt;
+    public Entry(DateTime date, string prompt, string response)
+    {
+        _currentTime = date;
+        Response = response;
+        Prompt = prompt;
+    }
+    public void display()
+    {
+        Console.WriteLine($"{_currentTime.ToShortDateString()}");
+        Console.WriteLine($"{Prompt}");
+        Console.WriteLine($"{Response}");
+        Console.WriteLine();
+    }
+}
+
+
+
+
+
+
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Journal journal = new Journal();
+
+        bool running = true;
+        while (running)
+        {
+        Console.WriteLine("Your Journal");
+        Console.WriteLine("1. Write an Entry");
+        Console.WriteLine("2. View Entries");
+        Console.WriteLine("3. Save as file");
+        Console.WriteLine("4. Upload a file");
+        Console.WriteLine("5. Exit Your Journal");
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+        //----------separation bar-------------------
+        case "1":
+            string prompt = journal.prompts.Randomprompts();
+            Console.WriteLine(prompt);
+            string response = Console.ReadLine();
+            Entry e = new Entry(DateTime.Now,prompt,response);
+            journal.AddNewEntry(e);
+            break;
+
+            //----------separation bar-------------------
+        case "2":
+            journal.DisplayEntries();
+            break;
+       case "3":
+            Console.Write("Please enter a name: ");
+            string filename = Console.ReadLine();
+            journal.SaveasFile(filename);
+            break;
+        case "4":
+            Console.Write("Please enter a file name: ");
+            string f = Console.ReadLine();
+            journal.LoadaFile(f);
+            break;
+        case "5":
+            running = false;
+            break;
+        }
+        }
+
+
+
+
+
+
+
+
+
+
     }
+    
+
 }
