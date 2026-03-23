2b6bbc97e117087f04366de0b4ff64990a065339
Rosyauto7410209 <64661777+Rosyauto7410209@users.noreply.github.com>
2026-03-23 17:23:32 -0600

    commit

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -8,14 +8,14 @@ class Program
         Video video1 = new Video("Basics for you","Jimbo", 500);
         video1.AddComment(new Comment("john", "very helpful"));
 
-        Video video2 = new Video("Basics for you","Jimbo", 300);
-        video2.AddComment(new Comment("john", "very helpful"));
+        Video video2 = new Video("best snacks of 2020","slimjim", 300);
+        video2.AddComment(new Comment("realman", "I prefer number 3 over 2."));
 
-        Video video3 = new Video("Basics for you","Jimbo", 400);
-        video3.AddComment(new Comment("Jane", "very helpful"));
+        Video video3 = new Video("what is the best color?","crayola", 400);
+        video3.AddComment(new Comment("Jane", "My son prefers red over green."));
 
-        Video video4 = new Video("Basics for you","Jimbo", 100);
-        video4.AddComment(new Comment("Jerry", "very helpful"));                        
+        Video video4 = new Video("how to change your oil","CarMechanics", 100);
+        video4.AddComment(new Comment("Jerry", "Thanks for the video!"));                        
 
     foreach(Video video in videos)
         {
